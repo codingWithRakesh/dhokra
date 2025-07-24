3dc2a153f237f97cf3bef552965d52121d487038
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-07-25 01:09:43 +0530

    feat: initialize server with Express and essential middleware
    - Added package.json for server dependencies and scripts
    - Created app.js to set up Express server with CORS, cookie-parser, and static file serving
    - Introduced constants.js for database name and cookie options
    - Implemented multer middleware for file uploads
    - Created utility functions for API error handling and responses
    - Integrated Cloudinary for image uploads and management
    - Established directory structure with .gitkeep files for controllers, db, models, and routes

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173/",
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static("public"))
+app.use(cookieParser())
+
+import userRouter from "./routes/user.route.js"
+
+app.use("/user", userRouter)
+
+app.get("/", (req, res) => {
+    res.send("working")
+})
+
+
+export { app }
---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,44 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+
+const uploadOnCloudinary = async (base64Image) => {
+    try {
+        if(!base64Image) return null
+        const result = await cloudinary.uploader.upload(base64Image, {
+            resource_type : 'auto',
+          });      
+
+        return result;
+    } catch (error) {
+
+        return null;
+    }
+};
+
+const getPublicId = (url) => {
+    const parts = url.split("/")
+    const publicIdEx = parts[parts.length -1]
+    const publicId = publicIdEx.split(".")[0]
+    return publicId
+}
+
+const deleteFromCloudinary = async (publicId) => {
+    try {
+        if(!publicId) return null
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("successfully upload")
+        return result;
+    } catch (error) {
+        console.error('Error deleting from Cloudinary:', error);
+        return null
+    }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary, getPublicId };
