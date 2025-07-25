5b1fcbef764e294c1aa8446f587322aeaf8fc4e1
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-07-26 00:49:46 +0530

    feat: add helmet for security and update dependencies
    - Added helmet middleware to enhance security in app.js
    - Updated package.json and package-lock.json to include new dependencies: helmet, razorpay, sharp, and uuid

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import helmet from "helmet";
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -7,10 +8,15 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:5173/",
     credentials: true
 }))
+app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// app.use(helmet({
+//   contentSecurityPolicy: false
+// }));
 
 import userRouter from "./routes/user.route.js"
 
