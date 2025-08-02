a939054f62b15b04abfe2230d888c702e12a6b7b
SUJOY GARAI <sujoycode999@gmail.com>
2025-08-02 22:47:45 +0530

    ...

--- client/src/components/AvailableCollectionPage.jsx
+++ client/src/components/AvailableCollectionPage.jsx
@@ -58,7 +58,7 @@ const FeaturedCollectionSection = () => {
                   <img
                     src={product.image}
                     alt={product.name}
-                    className="w-full h-full object-contain object-center transition-transform duration-500 group-hover:scale-105 rounded"
+                    className="w-full h-full object-cover object-center transition-transform duration-500 group-hover:scale-105 rounded"
                     loading="lazy"
                   />
                 </div>
