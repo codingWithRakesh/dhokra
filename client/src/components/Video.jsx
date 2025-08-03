6465426d1cd3759862dded4e169bce0014bcf609
SUJOY GARAI <sujoycode999@gmail.com>
2025-08-03 15:01:29 +0530

    ....

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -106,7 +106,7 @@ export default function VideoGallery() {
           <div className="relative w-full max-w-6xl">
             {/* Navigation Arrows */}
             <button 
-              className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-12 bg-black/50 hover:bg-amber-600 text-white p-3 rounded-full z-50 transition-all"
+              className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-2 bg-black/50 hover:bg-amber-600 text-white p-3 rounded-full z-50 transition-all"
               onClick={() => navigate('prev')}
               aria-label="Previous video"
             >
@@ -114,7 +114,7 @@ export default function VideoGallery() {
             </button>
             
             {/* Video Container */}
-            <div className="h-[80vh] bg-black rounded-lg overflow-hidden">
+            <div className="h-[80vh] w-auto rounded-lg overflow-hidden">
               {isLoading ? (
                 <div className="w-full h-full flex items-center justify-center">
                   <div className="animate-pulse text-white">Loading...</div>
@@ -131,7 +131,7 @@ export default function VideoGallery() {
             </div>
 
             <button 
-              className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-12 bg-black/50 hover:bg-amber-600 text-white p-3 rounded-full z-50 transition-all"
+              className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-2 bg-black/50 hover:bg-amber-600 text-white p-3 rounded-full z-50 transition-all"
               onClick={() => navigate('next')}
               aria-label="Next video"
             >
